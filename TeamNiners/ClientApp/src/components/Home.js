f4eeb9f7fd7c2265b7e7257a39d025e58bc2f84f
rwing6924 <ryan_645@outlook.com>
2019-09-19 14:32:35 -0400

    Displayed API

--- TeamNiners/ClientApp/src/components/Home.js
+++ TeamNiners/ClientApp/src/components/Home.js
@@ -6,22 +6,35 @@ export class Home extends Component {
 
     constructor(props) {
     super(props);
-    this.state = { businessCity: "" };
+    this.state = { businessCity: "", email: "" };
         this.getData = this.getData.bind(this);
+        this.getBusinessLoginData = this.getBusinessLoginData.bind(this);
   }
 
     getData() {
-    
+
+        console.log("test");
+        axios.get('http://localhost:54047/api/APIBusinesses')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    businessCity: res.data[0].businessCity
+                });
+            })
+    }
+
+    getBusinessLoginData() {
+
     console.log("test");
-    axios.get('http://localhost:54047/api/APIBusinesses')
+            axios.get('http://localhost:54047/api/BusinessLogins')
         .then(res => {
             console.log(res.data);
             this.setState({
-                businessCity: res.data[0].businessCity
+                email: res.data[0].email
             });
         })
-    
-  }
+
+    }
 
 
     myFunction = () => {
@@ -52,7 +65,8 @@ export class Home extends Component {
         </ul>
             <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
             <button onClick={this.getData}>Click Me</button>
-            <p>{this.state.businessCity}</p>
+            <button onClick={this.getBusinessLoginData}>Click Me</button>
+            <p>{this.state.businessCity} AND the Email is {this.state.email}</p>
 
         </div>
     );
