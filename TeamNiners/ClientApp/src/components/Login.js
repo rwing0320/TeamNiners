97fcb07394757383709340f9b1630089a04c2e62
rwing6924 <ryan_645@outlook.com>
2019-09-23 16:53:38 -0400

    put in login page

---
+++ TeamNiners/ClientApp/src/components/Login.js
@@ -0,0 +1,78 @@
+﻿import React, { Component } from 'react';
+import axios from 'axios';
+
+export class Login extends Component {
+    displayName = Login.name
+
+    constructor(props) {
+        super(props);
+        this.state = { email: "", password: "" };
+        this.setEmail = this.setEmail.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+        //this.getData = this.getData.bind(this);
+        //this.getBusinessLoginData = this.getBusinessLoginData.bind(this);
+    }
+
+    setEmail(event) {
+        //this.state.email = value
+        
+        this.state.email = event.target.value
+        console.log(this.state.email);
+    }
+
+    setPassword(event) {
+        this.state.password = event.target.value
+        console.log(this.state.password);
+    }
+    //getData() {
+
+    //    console.log("test");
+    //    axios.get('http://localhost:54047/api/APIBusinesses')
+    //        .then(res => {
+    //            console.log(res.data);
+    //            this.setState({
+    //                businessCity: res.data[0].businessCity
+    //            });
+    //        })
+    //}
+
+    //getBusinessLoginData() {
+
+    //    console.log("test");
+    //    axios.get('http://localhost:54047/api/BusinessLogins')
+    //        .then(res => {
+    //            console.log(res.data);
+    //            this.setState({
+    //                email: res.data[0].email
+    //            });
+    //        })
+
+    //}
+
+    getBusinessLogin() {
+
+    }
+
+    render() {
+        return (
+
+            <form>
+                <h1>Hello</h1>
+                <p>Enter your name:</p>
+                <input
+                    type="text"
+                    name="username"
+                   
+                    onChange={this.setEmail}
+                />
+                <p>Password</p> 
+                <input type="password" name="psw"  onChange={this.setPassword} />
+
+                
+            </form>
+
+
+       
+        );
+    }
+}
---
+++ TeamNiners/Context/BlogContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TeamNiners.Models
+{
+    public partial class BlogContext : DbContext
+    {
+        public BlogContext()
+        {
+        }
+
+        public BlogContext(DbContextOptions<BlogContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=dbo_Niners;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
